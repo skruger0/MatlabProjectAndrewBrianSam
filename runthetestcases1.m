69c459e0dd3ee4e7fc774723d340e323e31ffc26
BRIAN MICKEL <brianemickel@gmail.com>
2016-03-20 23:01:49 -0700

    "test case run "

---
+++ runthetestcases1.m
@@ -0,0 +1,74 @@
+max_beats = 12
+target_formation = tiny_corners_to_center_target;
+initial_formation = tiny_corners_to_center_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = tiny_block_traverses_target;
+initial_formation = tiny_block_traverses_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = tiny_block_splits_target;
+initial_formation = tiny_block_splits_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = tiny_block_regroups_target;
+initial_formation = tiny_block_regroups_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = small_line_to_uparrow_target;
+initial_formation = small_line_to_uparrow_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = small_blocks_to_circles_target;
+initial_formation = small_blocks_to_circles_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = small_blocks_approach_target;
+initial_formation = small_blocks_approach_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+max_beats = 12;
+target_formation = medium_triangle_to_box_target;
+initial_formation = medium_triangle_to_box_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+target_formation = medium_e7_to_happy_target;
+initial_formation = medium_e7_to_happy_initial;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+max_beats = 90;
+initial_formation = fullscale_bridge_to_train_initial;
+target_formation = fullscale_bridge_to_train_target;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+initial_formation = huge_script_cal_initial;
+target_formation = huge_script_cal_target;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
+
+initial_formation = large_go_bears_initial;
+target_formation = large_go_bears_target;
+[instructions] = calband_transition(initial_formation, target_formation, max_beats);
+calband_visualize_transition_v01(initial_formation, target_formation, instructions, max_beats)
+
